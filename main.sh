db8f5698917949779881ae7040fa8f34ee8638d2
nahjh2 <nahjh2@gmail.com>
2023-07-08 15:31:31 +0900

    add git.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ echo -------ENV-END-------
 
 sudo apt update
 sudo ./scripts/ufw.sh
+sudo ./scripts/git.sh
 sudo ./scripts/nginx.sh
 sudo ./scripts/certbot.sh
 sudo ./scripts/code-server.sh
---
+++ scripts/git.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git-all
+
+git version
+
+git config.user.name = $GIT_USER_NAME
+git config.user.email = $EMAIL
