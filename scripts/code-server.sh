94ce40cb996ee71f93d3d2a64d8a74f9a60c87e7
nahjh2 <nahjh2@gmail.com>
2023-07-08 14:28:08 +0900

    remove config.yaml

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -3,9 +3,6 @@ curl -fsSL https://code-server.dev/install.sh | sh
 # https://coder.com/docs/code-server/latest/guide#using-lets-encrypt-with-nginx
 cp ../configs/code-server/nginx/sites-available /etc/nginx/sites-available/code-server
 sudo ln -s /etc/nginx/sites-available/code-server /etc/nginx/sites-enabled/code-server
-
-cp ../configs/code-server/config.yaml ~/.config/code-server/config.yaml
-
 sudo certbot --non-interactive --redirect --agree-tos --nginx -d "vscode.${SERVER_DOMAIN}" -m $EMAIL
 
 sudo systemctl enable --now code-server@$USER
