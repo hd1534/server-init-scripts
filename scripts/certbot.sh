0fc97a5c36190a4a00d19cba389879acbab782c3
nahjh2 <nahjh2@gmail.com>
2023-07-08 13:03:16 +0900

    add env && cronjob

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,1 +1,4 @@
 sudo apt install -y certbot python3-certbot-nginx
+sudo certbot --non-interactive --redirect --agree-tos --nginx -d $SERVER_DOMAIN -m me@example.com
+
+echo "\r\n0 0 1 * * certbot renew" >> /etc/crontab
--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -5,4 +5,4 @@ sudo systemctl enable --now code-server@$USER
 # https://coder.com/docs/code-server/latest/guide#using-lets-encrypt-with-nginx
 cp ../configs/code-server/nginx/sites-available /etc/nginx/sites-available/code-server
 sudo ln -s /etc/nginx/sites-available/code-server /etc/nginx/sites-enabled/code-server
-sudo certbot --non-interactive --redirect --agree-tos --nginx -d contabo.nahjh2.com -m me@example.com
+sudo certbot --non-interactive --redirect --agree-tos --nginx -d "vscode.${SERVER_DOMAIN}" -m $EMAIL
